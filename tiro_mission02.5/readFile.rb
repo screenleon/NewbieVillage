a0b9cf37e52f562dfd209afa9711b98205739e8e
screenleon <screen.leon@gmail.com>
2019-04-14 15:25:55 +0800

    read file print limit
    read file and print at most 10 line

--- tiro_mission02.5/readFile.rb
+++ tiro_mission02.5/readFile.rb
@@ -1,11 +1,15 @@
 puts "What file you want to print?"
 file_path = gets
 file_path.delete!("\n")
-
+lineCount = 1
 begin
   File.open(file_path, "r") do |f|
     f.each_line do |line|
       puts line
+      lineCount += 1
+      if lineCount > 10 then
+        break
+      end
     end
   end
 rescue Exception => e
